77dea635d529b247e0a6e9eb12fd1278fa3e4cc0
Bernat Romagosa <bromagosa@dosta.net>
2015-01-13 13:36:46 +0100

    tutorial started

--- snapi/morphic.js
+++ snapi/morphic.js
@@ -21,3 +21,43 @@ SpeechBubbleMorph.prototype.showUp = function (stage, pos) {
 	this.adjustPosition(pos);
 }
 
+var TriangleBoxMorph;
+
+TriangleBoxMorph.prototype = new Morph();
+TriangleBoxMorph.prototype.constructor = TriangleBoxMorph;
+TriangleBoxMorph.uber = Morph.prototype;
+
+function TriangleBoxMorph(orientation) {
+	this.init(orientation);
+}
+
+TriangleBoxMorph.prototype.init = function (orientation) {
+    TriangleBoxMorph.uber.init.call(this);
+    this.orientation = orientation ? orientation : 'left';
+    this.setExtent(new Point(20, 28));
+}
+
+TriangleBoxMorph.prototype.drawNew = function () {
+    var context,
+		ext,
+        myself = this;
+
+	this.image = newCanvas(this.extent());
+    context = this.image.getContext('2d');
+
+	context.fillStyle = myself.color.toString();
+	context.beginPath();
+
+	if (this.orientation == 'left') {
+		context.moveTo(0,  14);
+	    context.lineTo(20, 0);
+    	context.lineTo(20, 28);	
+	} else if (this.orientation == 'right') {
+		context.moveTo(0,  0);
+	    context.lineTo(20, 14);
+    	context.lineTo(0,  28);	
+	}
+
+	context.closePath();
+	context.fill();
+};
---
+++ snapi/widgets.js
@@ -0,0 +1,92 @@
+PushButtonMorph.prototype.disable = function() {
+	this.labelColor = new Color(180,180,180);
+	this.createLabel();
+	this.fixLayout();
+	this.action = null;
+}
+
+DialogBoxMorph.prototype.originalPopUp = DialogBoxMorph.prototype.popUp;
+DialogBoxMorph.prototype.popUp = function (world) {
+	this.originalPopUp(world);
+	this.show();
+	if (this.popUpPosition) { 
+		this.setPosition(this.popUpPosition);
+		if (this.arrow) { this.placeArrow(); }
+	}
+};
+
+DialogBoxMorph.prototype.tutorialWindow = function (
+    title,
+    pic,
+    msg,
+	popUpPosition,
+	arrowOrientation,
+	previousWindow,
+	nextWindow,
+	cancelAction
+) {
+    var bdy = new AlignmentMorph('column', this.padding);
+
+    function labelText(string) {
+        return new TextMorph(
+            localize(string),
+            12,
+            null, // style
+            false, // bold
+            null, // italic
+            null, // alignment
+            null, // width
+            null, // font name
+            MorphicPreferences.isFlat ? null : new Point(1, 1),
+            new Color(255, 255, 255) // shadowColor
+        );
+    }
+
+	this.popUpPosition = popUpPosition;
+	this.cancelAction = cancelAction;
+
+    bdy.setColor(this.color);
+
+    if (msg) {
+        bdy.add(labelText(msg));
+    }
+
+    bdy.fixLayout();
+
+    this.labelString = title;
+    this.createLabel();
+    if (pic) {this.setPicture(pic); }
+
+    this.addBody(bdy);
+
+    bdy.fixLayout();
+
+	this.previousButton = this.addButton(previousWindow, '« Previous');
+	if (!previousWindow) { this.previousButton.disable() };
+
+    this.addButton(this.cancelAction, 'Quit');
+
+	this.nextButton = this.addButton(nextWindow, 'Next »');
+	if (!nextWindow) { this.nextButton.disable() };
+
+    this.fixLayout();
+    this.drawNew();
+    this.fixLayout();
+
+	this.arrow = new TriangleBoxMorph(arrowOrientation);
+	this.arrow.setColor(this.titleBarColor);
+	if (arrowOrientation) { this.add(this.arrow) };
+
+	this.placeArrow = function() {
+		if (this.arrow.orientation == 'left') {
+			this.arrow.setLeft(this.left() - 20);
+			this.moveBy(new Point(20, -14));
+		} else if (this.arrow.orientation == 'right') {
+			this.arrow.setRight(this.right() + 20);
+			this.moveBy(new Point(- this.extent().x - 20, -14));
+		}
+	}
+
+	return this;
+};
+
