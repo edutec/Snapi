6f9d77dc776672b9b50edee90994d01386509cdc
Bernat Romagosa <bromagosa@dosta.net>
2015-01-19 11:47:15 +0100

    About box updated

--- snapi/morphic.js
+++ snapi/morphic.js
@@ -1,3 +1,35 @@
+Morph.fromImageURL = function(url) {
+	var m = new Morph();
+
+	m.texture = url;
+
+	m.drawNew = function() {
+		this.image = newCanvas(this.extent());
+		var context = this.image.getContext('2d');
+		context.fillStyle = 'rgba(0,0,0,0)';
+		context.fillRect(0, 0, this.width(), this.height());
+		if (this.texture) {
+			this.drawTexture(this.texture);
+		}
+	}
+
+	m.drawCachedTexture = function () {
+   		var context = this.image.getContext('2d');
+       	context.drawImage(
+       	    this.cachedTexture,
+       	    0,
+       	    Math.round((this.height() - this.cachedTexture.height) / 2)
+       	);
+       	this.changed();
+   	};
+
+	m.drawNew();
+
+   	m.setExtent(new Point(m.cachedTexture.width, m.cachedTexture.height));
+
+	return m;
+}
+
 SpeechBubbleMorph.prototype.adjustPosition = function(pos) {
 	var oldPos = this.position(),
 		newPos = pos.subtract(new Point(0, this.height()));
